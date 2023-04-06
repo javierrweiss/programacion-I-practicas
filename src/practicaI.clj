560b4bde6ded41278d9396a05517aab8b0f50230
José Javier Blanco Rivero <56279998+javierrweiss@users.noreply.github.com>
2023-04-06 16:07:19 +0000

    Initial commit

---
+++ src/practicaI.clj
@@ -0,0 +1,55 @@
+(ns practicaI
+  (:require [clojure.set :as set]))
+
+;; 0. Declare una variable con su nombre y apellido. Escriba en un comentario qué 
+;;    tipo de dato ha empleado.
+
+
+;; 1. Cree un vector de números positivos impares hasta el número 21
+
+
+
+;; 2. Registre los jugadores de dos equipos de basket y dos de fútbol 5. 
+;;    Al menos 3 jugadores juegan fútbol y basket. Utilice conjuntos
+
+
+
+;; 2.1 Desde la Dirección de Deportes nos piden una lista (lo que no implica 
+;;     devolver esta estructura de datos) de todos los jugadores de ambos
+;;     deportes. PISTA: utilizar la función (set/union) -ya el namespace está
+;;     requerido arriba con el alias 'set'.
+
+
+
+;; 2.2 Identifique los jugadores que participan en ambas disciplinas. PISTA: 
+;;     Existe una función de conjuntos en el namespace 'clojure.set' para resolver este problema.
+
+
+
+
+;; 2.3 Identifique los jugadores que juegan fútbol, pero no basket. PISTA:
+;;     Existe una función de conjuntos en el namespace 'clojure.set' para resolver este problema.
+
+
+;; 3. Cree un mapa con los 5 principales indicadores provistos en la página del INDEC
+;;    (https://www.indec.gob.ar/), a saber, población, desocupación, etc. 
+
+
+
+;; 3.1 Utilice la función (juxt) para obtener los valores correspondientes a las llaves 
+;;     que haya elegido para los indicadores 'Tasa de desocupación' e 'Índice de producción industrial 
+;;     manufacturero'. 
+;;     NOTA: Al ubicar el cursor sobre la función, podrá ver una ventana con la documentación y ejemplos.
+
+
+
+;; 3.2 Utilice la función (select-keys) para obtener un mapa con el par llave-valor 
+;;     con las llaves que haya elegido para los indicadores 'Población' y 'Precios al consumidor'.
+
+
+(comment 
+;; Utilice este espacio para experimentar con el código. Luego, cuando esté seguro que el 
+;; código trabaja correctamente, cópielo a su lugar correspondiente arriba.   
+  
+
+)
