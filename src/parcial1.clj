a55abf2fb6d7bb7d0863c256bde5a2ca993eef8d
javierrweiss <javierweiss@gmail.com>
2023-05-11 02:56:07 +0000

    Segunda pregunta con sus tests

--- src/parcial1.clj
+++ src/parcial1.clj
@@ -1,26 +1,41 @@
 (ns parcial1)
 
-;; En esta evaluación se encontrará con un conjunto de funciones que deberá completar de acuerdo a su premisa respectiva. Así que tómese la 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ¡¡¡¡LEA CON ATENCIÓN!!! ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; I. En esta evaluación se encontrará con un conjunto de funciones que deberá completar de acuerdo a su premisa respectiva. Así que tómese la 
 ;; libertad de modificar el cuerpo de la función según lo considere apropiado.
 ;; De igual modo, siempre que lo estime conveniente puede crear otras funciones que sirvan de auxiliares. Eso sí, nunca modifique el nombre 
 ;; ni la cantidad de argumentos de las funciones a completar.
-;; Cada función cuenta con un conjunto de tests que verifican que trabaje correctamente. Oprima las teclas Ctrl + Alt + c luego Shift + t
+;; Puede importar y requerir las librerías que juzgue convenientes. Agréguelas en el archivo deps.edn de ser necesario y reinicie el REPL.
+;; II. Cada función cuenta con un conjunto de tests que verifican que trabaje correctamente. Oprima las teclas Ctrl + Alt + c luego Shift + t
 ;; para correr los tests. Alternativamente, puede presionar F1, escribir calva y buscar en el menú de comandos la opción "Run All Tests".
 ;; Si todos los tests resultan aprobados, usted habrá aprobado la evaluación con la máxima calificación. Puede correr los tests la cantidad
 ;; de veces que necesite.
 ;; No tiene permitido bajo ninguna circunstancia modificar el archivo de tests. Caso contrario quedará reprobado.
-;; Utilice la forma (comment ) para explorar las respuestas y deje allí las cosas que intentó. Se considerará en la evaluación. 
+;; III. Utilice la forma (comment ) para explorar las respuestas y deje allí las cosas que intentó. Se considerará en la evaluación. 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; COMPLETE LAS SIGUIENTES FUNCIONES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; 1. La siguiente función recibirá un vector de strings y deberá devolver un vector de booleanos, donde cada uno marcará true si la palabra 
-;;  ubicada en tal posición es un diptongo o un hiato. En caso contrario, marcará false. 
+;;    ubicada en tal posición es un diptongo o un hiato. En caso contrario, marcará false. 
 ;; PISTA: Convendría definir las vocales; el namespace clojure.set puede ser de ayuda.
 
 (defn obtener-diptongos-y-hiatos
   [palabras]
   nil)
 
-;; 2. 
+;; 2. Necesitamos calcular las posiciones de los equipos del campeonato local de fútbol. La siguiente función recibirá un vector de mapas
+;;    donde cada mapa tendrá la siguiente forma {:local "EquipoA" :goles-local 1 :visitante "EquipoB" :goles-visitante 2}. Calcule los puntos
+;;    de cada equipo y devuelva un vector de mapas ordenado con el equipo con mayor puntaje de primero de la siguiente forma 
+;;    [{:equipo "EquipoA" :puntos 23} {:equipo "EquipoZ" :puntos 3} ...]
 
+(defn calcula-posiciones
+  [lista-resultados]
+  nil)
 
 
 
--- test/parcial1test.clj
+++ test/parcial1test.clj
@@ -25,5 +25,25 @@
           resultado [false false]]
       (is (= resultado (obtener-diptongos-y-hiatos input))))))
 
+(deftest posiciones-en-la-tabla
+  (let [resultados [{:local "Atlanta" :goles-local 3 :visitante "Newell's" :goles-visitante 3}
+                    {:local "Real Madrid" :goles-local 2 :visitante "Manchester City" :goles-visitante 3}
+                    {:local "Boca Juniors" :goles-local 2 :visitante "River Plate" :goles-visitante 6}
+                    {:local "Atlanta" :goles-local 1 :visitante "Real Madrid" :goles-visitante 3}
+                    {:local "River Plate" :goles-local 4 :visitante "Manchester City" :goles-visitante 5}
+                    {:local "Boca Juniors" :goles-local 0 :visitante "Newell's" :goles-visitante 0}]
+        posiciones (calcula-posiciones resultados)]
+    (testing "El resultado es un vector y contiene mapas con la forma correcta"
+      (is (and (vector? posiciones) (apply (every-pred map? #(= (keys %) (list :equipo :puntos))) posiciones)))) 
+    (testing "Manchester City es el primero con 6 puntos"
+      (is (= {:equipo "Manchester City" :puntos 6} (first posiciones))))
+    (testing "Boca Juniors o Atalanta está de último con 1 punto"
+      (is (or (= {:equipo "Boca Juniors" :puntos 1} (last posiciones))
+              (= {:equipo "Atlanta" :puntos 1} (last posiciones)))))))
+ 
 
-
+(comment 
+ (run-all-tests)
+  
+  
+) 
